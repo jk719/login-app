5079eb0ee0252598fe9634731a6ff0397ae84d58
Jamil Kabir <jamilkabir719@gmail.com>
2023-11-08 16:25:49 -0500

    Second Commit

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // for bcrypt
+const { Pool } = require('pg');
 
 const app = express();
+
+// PostgreSQL pool configuration
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-  secret: 'your_secret_key',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }));
@@ -20,28 +33,44 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => {
-  console.log('Server started on http://localhost:3000');
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+    
+    if (user.rows.length > 0) {
+      const match = await bcrypt.compare(password, user.rows[0].password);
+      
+      if (match) {
+        req.session.user = user.rows[0];
+        res.redirect('/dashboard');
+      } else {
+        res.redirect('/?error=Invalid credentials');
+      }
+    } else {
+      res.redirect('/?error=User not found');
+    }
+  } catch (error) {
+    console.error(error);
+    res.redirect('/?error=Login failed');
+  }
 });
 
+app.post('/signup', async (req, res) => {
+  const { username, password, email } = req.body;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-
-
-// Dummy user for example
-const user = {
-  username: 'user1',
-  password: 'password123' // In a real app, this should be hashed
-};
-
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  
-  // For demo purposes, we'll just check if the credentials match the dummy user
-  if (username === user.username && password === user.password) {
-    req.session.user = user;
+  try {
+    const newUser = await pool.query(
+      'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id',
+      [username, email, hashedPassword]
+    );
+    req.session.user = { id: newUser.rows[0].id, username, email };
     res.redirect('/dashboard');
-  } else {
-    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.redirect('/?error=Sign up failed');
   }
 });
 
@@ -49,13 +78,18 @@ app.get('/dashboard', (req, res) => {
   if (req.session.user) {
     res.render('dashboard', { user: req.session.user });
   } else {
-    res.redirect('/');
+    res.redirect('/?error=You must log in first');
   }
 });
 
-
 app.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.redirect('/');
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
 });
 
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
